91113f077c3af340b9a172dc4fca740e7ea26527
Guido Leandro Maliandi <guido@unboundvr.com>
2015-09-20 19:21:13 -0300

    fixing merge error

--- client/app/App.js
+++ client/app/App.js
@@ -1,95 +1,47 @@
 'use strict';
 
-define(['Three', 'FirstPersonControls', 'Renderer', 'DomContainer', 'Scene', 'PlayerSync', 'loaders/ObjectLoader', 'loaders/GUILoader', 'loaders/ScriptsLoader', 'Reticle', 'PointerLock', 'QueryString', 'KeyVR'],
-    function(THREE, fpControls, renderer, container, scene, playerSync, objectLoader, guiLoader, scriptsLoader, reticle, pointerLock, queryString, keyVR) {
+define(['Three', 'Detector', 'Renderer', 'DomContainer', 'Scene', 'loaders/ObjectLoader', 'loaders/GUILoader', 'loaders/ScriptsLoader', 'QueryString', 'World'],
+    function(THREE, detector, renderer, container, scene, objectLoader, guiLoader, scriptsLoader, queryString, world) {
+        // TODO use same style as other objects
         var App = function() {
-            var camera;
-            var prevTime;
-            var request;
-
             this.load = function(json) {
                 scene.setScene(objectLoader.parse(json.scene));
-                scene.setCSS3DScene(guiLoader.parse(json.gui));
+                scene.setCSS3DScene(guiLoader.parse(json));
 
                 // Perhaps we could pass more things -- the most important thing is that later on we document which things we expose to scripts
-                // We can also pass things like renderer directly as params (such as when we pass scene) but I think it's OK to pass just scene as a distinct param
+                // We can also pass other things like renderer directly as params (such as when we pass scene) but I think it's OK to pass just scene as a distinct param
                 var relevantApp = {
-                    setCamera: this.setCamera,
-                    setSize: this.setSize,
-                    play: this.play,
-                    stop: this.stop,
-                    renderer: renderer,
-                    playerSync: playerSync
+                    setCamera: renderer.setCamera,
+                    play: world.play,
+                    stop: world.stop,
+                    renderer: renderer.webGL
                 };
 
-                scriptsLoader.load(json.scripts, relevantApp);
-                this.setCamera(fpControls.camera);
-            };
-
-            this.setCamera = function(value) {
-                camera = value;
-                camera.aspect = this.width / this.height;
-                camera.updateProjectionMatrix();
-            };
-
-            this.setSize = function(width, height) {
-                this.width = width;
-                this.height = height;
-
-                camera.aspect = this.width / this.height;
-                camera.updateProjectionMatrix();
-
-                renderer.setSize(width, height);
-            };
-
-            var animate = function(time) {
-                request = requestAnimationFrame(animate);
-                scriptsLoader.dispatchEvent(scriptsLoader.events.update, {time: time, delta: time - prevTime});
-                fpControls.animate();
-                reticle.loop();
-                renderer.render(scene, camera);
-                prevTime = time;
-            };
-
-            this.play = function() {
-                scriptsLoader.addEventListeners();
-
-                request = requestAnimationFrame(animate);
-                prevTime = performance.now();
-            };
-
-            this.stop = function() {
-                scriptsLoader.removeEventListeners();
-
-                cancelAnimationFrame(request);
+                scriptsLoader.load(json, relevantApp);
             };
 
             this.init = function() {
+                if (!detector.webgl) {
+                    detector.addGetWebGLMessage();
+                    return;
+                }
+    
                 var _this = this;
 
                 var remoteLoader = new THREE.XHRLoader();
                 
                 var sceneId = queryString.sceneId;
+                var url = sceneId ? 'api/scene/' + sceneId : 'api/scene';
                 
-                remoteLoader.load('client/assets/'+ sceneId +'.json', function(text) {
+                remoteLoader.load(url, function(text) {
                     _this.load(JSON.parse(text));
-                    _this.setSize(window.innerWidth, window.innerHeight);
-                    _this.play();
-	
-                    fpControls.init();
-                    playerSync.init();
-                    reticle.init();
-                    pointerLock.init();
-                    keyVR.init();
 
-                    container.appendChild(renderer.domElement);
-
-                    window.addEventListener('resize', function() {
-                        _this.setSize(window.innerWidth, window.innerHeight);
-                    });
+                    world.start();
+                    
+                    container.appendChild(renderer.getDomElement());
                 });
             };
         };
 
-        return new App();
+        new App().init();
     });
