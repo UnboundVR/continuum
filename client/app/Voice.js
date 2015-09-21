7665d00c055b33563b04192ceef972dfa7d6774f
Mauro Julián Fernández <mauro@unboundvr.com>
2015-09-20 23:23:18 -0300

    WIP voice

--- client/app/Voice.js
+++ client/app/Voice.js
@@ -4,11 +4,11 @@ define(['SocketIO', 'RTCMultiConnection', 'World'], function(io, rtc, world) {
 
     var init = function() {
         var port = '1338';
-        var rtcMultiConnection = new rtc();
-        var SIGNALING_SERVER = 'http://' + document.domain + ':' + port;
+        var connection = new rtc('continuum');
+        var SIGNALING_SERVER = 'http://localhost:1338';
         var socket = io.connect(SIGNALING_SERVER);
 
-        rtcMultiConnection.session = {
+        /*rtcMultiConnection.session = {
             audio: true,
             video: false,
             screen: false,
@@ -22,11 +22,15 @@ define(['SocketIO', 'RTCMultiConnection', 'World'], function(io, rtc, world) {
             OfferToReceiveVideo: false
         };
 
+        rtcMultiConnection.channel = 'continuum';
+        rtcMultiConnection.open();
+
         rtcMultiConnection.openSignalingChannel = function(config) {
+            console.log('bla');
             var channel = 'continuum';
             var user = Math.round(Math.random() * 9999) + 9999;
 
-            io.connect(SIGNALING_SERVER).emit('new-connection', {
+            socket.emit('new-connection', {
                 channel: channel,
                 user: user
             });
@@ -39,9 +43,10 @@ define(['SocketIO', 'RTCMultiConnection', 'World'], function(io, rtc, world) {
             };
 
             socket.on('message', config.onmessage);
-        };
+        };*/
 
-        rtcMultiConnection.open('continuum');
+
+
     };
 
     world.onInit(init);
--- voice_server.js
+++ voice_server.js
@@ -4,11 +4,11 @@ var app = require('express');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-io.on('new-connection', function(socket) {
-    console.log('New connection from ' + socket.user);
+io.on('connect', function(socket) {
+    console.log('New connection');
 });
 
 var port = 1338;
 server.listen(port, function() {
-    console.log('Voice server listeining at port ' + port + '!');
+    console.log('Voice server listening at port ' + port + '!');
 });
