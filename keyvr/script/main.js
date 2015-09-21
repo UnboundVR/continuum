b469ed97dd352a7a69953b8073eb56cb9338c0a1
Guido Leandro Maliandi <guido@unboundvr.com>
2015-09-21 20:31:29 -0300

    adding a custom url field to keyvr + refactors to simplify development

--- keyvr/script/main.js
+++ keyvr/script/main.js
@@ -5,17 +5,24 @@ var socket = io.connect(window.location.origin + '/keyvr', {
 
 // As soon as we connect to node, we get assigned an ID
 socket.on('connect', function() {
-	hookQRButton(window.location.origin + '/world?keyboardId=' + this.id);
-	
-	console.log(this.id);
+	hookQRButton(this.id);
 });
 
 // After we have an ID, we can enable the "Generate QR" button, since the QR is generated based on this ID
-function hookQRButton(payload) {
+function hookQRButton(keyboardId) {
 	$(function() {
-		$('#dasButton').attr('disabled', null).click(function() {
-			$('#qrCode').qrcode(payload);
-			showScreen('linkDevice');
+		$('#dasButton').attr('disabled', null);
+         
+        $('#dasForm').submit(function(e) {
+            e.preventDefault();
+            
+            var customAddress = $('#customAddress').val();
+            var payload = (customAddress || window.location.origin) + '/world?keyboardId=' + keyboardId;
+			
+            $('#qrCode').qrcode(payload);
+            $('#localLink').attr('href', payload);
+			
+            showScreen('linkDevice');
 		});
 	});
 }
@@ -50,6 +57,8 @@ socket.on('deviceConnected', function(data) {
     });
 	
 	showScreen('deviceLinked');
+    
+    
 });
 
 // Simple function to display a div and hide others
