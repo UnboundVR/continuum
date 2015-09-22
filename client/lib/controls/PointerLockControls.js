0098b8a8fda31f02a9cfc0a86842aa23675c2fbf
Guido Leandro Maliandi <guido@unboundvr.com>
2015-09-21 22:04:34 -0300

    undo jscs-ing of libraries

--- client/lib/controls/PointerLockControls.js
+++ client/lib/controls/PointerLockControls.js
@@ -3,71 +3,71 @@
  */
 
 // Modified to support calling a move function directly (for KeyVR)
-THREE.PointerLockControls = function(camera) {
+THREE.PointerLockControls = function ( camera ) {
 
-    var scope = this;
+	var scope = this;
 
-    camera.rotation.set(0, 0, 0);
+	camera.rotation.set( 0, 0, 0 );
 
-    var pitchObject = new THREE.Object3D();
-    pitchObject.add(camera);
+	var pitchObject = new THREE.Object3D();
+	pitchObject.add( camera );
 
-    var yawObject = new THREE.Object3D();
-    yawObject.position.y = 10;
-    yawObject.add(pitchObject);
+	var yawObject = new THREE.Object3D();
+	yawObject.position.y = 10;
+	yawObject.add( pitchObject );
 
-    var PI_2 = Math.PI / 2;
-
+	var PI_2 = Math.PI / 2;
+    
     var _this = this;
-
+    
     this.move = function(movement) {
         yawObject.rotation.y -= movement.x * 0.002;
-        pitchObject.rotation.x -= movement.y * 0.002;
+		pitchObject.rotation.x -= movement.y * 0.002;
 
-        pitchObject.rotation.x = Math.max(-PI_2, Math.min(PI_2, pitchObject.rotation.x));
+		pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, pitchObject.rotation.x ) );
     };
 
-    var onMouseMove = function(event) {
+	var onMouseMove = function ( event ) {
 
-        if (scope.enabled === false) return;
+		if ( scope.enabled === false ) return;
 
-        var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-        var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
 
-        _this.move({
-            x: movementX,
+		_this.move({
+            x: movementX, 
             y: movementY
         });
 
-    };
+	};
 
-    document.addEventListener('mousemove', onMouseMove, false);
+	document.addEventListener( 'mousemove', onMouseMove, false );
 
-    this.enabled = false;
+	this.enabled = false;
 
-    this.getObject = function() {
+	this.getObject = function () {
 
-        return yawObject;
+		return yawObject;
 
-    };
+	};
 
-    this.getDirection = function() {
+	this.getDirection = function() {
 
-        // assumes the camera itself is not rotated
+		// assumes the camera itself is not rotated
 
-        var direction = new THREE.Vector3(0, 0, -1);
-        var rotation = new THREE.Euler(0, 0, 0, 'YXZ');
+		var direction = new THREE.Vector3( 0, 0, -1 );
+		var rotation = new THREE.Euler( 0, 0, 0, "YXZ" );
 
-        return function(v) {
+		return function( v ) {
 
-            rotation.set(pitchObject.rotation.x, yawObject.rotation.y, 0);
+			rotation.set( pitchObject.rotation.x, yawObject.rotation.y, 0 );
 
-            v.copy(direction).applyEuler(rotation);
+			v.copy( direction ).applyEuler( rotation );
 
-            return v;
+			return v;
 
-        }
+		}
 
-    }();
+	}();
 
 };
--- client/lib/effects/StereoEffect.js
+++ client/lib/effects/StereoEffect.js
@@ -7,136 +7,134 @@
  * Off-axis stereoscopic effect based on http://paulbourke.net/stereographics/stereorender/
  */
 
-THREE.StereoEffect = function(renderer) {
+THREE.StereoEffect = function ( renderer ) {
 
-    // API
+	// API
 
-    var scope = this;
+	var scope = this;
 
-    this.eyeSeparation = 3;
-    this.focalLength = 15; 	// Distance to the non-parallax or projection plane
+	this.eyeSeparation = 3;
+	this.focalLength = 15; 	// Distance to the non-parallax or projection plane
 
-    Object.defineProperties(this, {
-        separation: {
-            get: function() {
-                return scope.eyeSeparation;
-            },
+	Object.defineProperties( this, {
+		separation: {
+			get: function () {
+				return scope.eyeSeparation;
+			},
+			set: function ( value ) {
+				console.warn( 'THREE.StereoEffect: .separation is now .eyeSeparation.' );
+				scope.eyeSeparation = value;
+			}
+		},
+		targetDistance: {
+			get: function () {
+				return scope.focalLength;
+			},
+			set: function ( value ) {
+				console.warn( 'THREE.StereoEffect: .targetDistance is now .focalLength.' );
+				scope.focalLength = value;
+			}
+		}
+	} );
 
-            set: function(value) {
-                console.warn('THREE.StereoEffect: .separation is now .eyeSeparation.');
-                scope.eyeSeparation = value;
-            }
-        },
-        targetDistance: {
-            get: function() {
-                return scope.focalLength;
-            },
+	// internals
 
-            set: function(value) {
-                console.warn('THREE.StereoEffect: .targetDistance is now .focalLength.');
-                scope.focalLength = value;
-            }
-        }
-    });
+	var _width, _height;
 
-    // internals
+	var _position = new THREE.Vector3();
+	var _quaternion = new THREE.Quaternion();
+	var _scale = new THREE.Vector3();
 
-    var _width, _height;
+	var _cameraL = new THREE.PerspectiveCamera();
+	var _cameraR = new THREE.PerspectiveCamera();
 
-    var _position = new THREE.Vector3();
-    var _quaternion = new THREE.Quaternion();
-    var _scale = new THREE.Vector3();
+	var _fov;
+	var _outer, _inner, _top, _bottom;
+	var _ndfl, _halfFocalWidth, _halfFocalHeight;
+	var _innerFactor, _outerFactor;
 
-    var _cameraL = new THREE.PerspectiveCamera();
-    var _cameraR = new THREE.PerspectiveCamera();
+	// initialization
 
-    var _fov;
-    var _outer, _inner, _top, _bottom;
-    var _ndfl, _halfFocalWidth, _halfFocalHeight;
-    var _innerFactor, _outerFactor;
+	renderer.autoClear = false;
 
-    // initialization
+	this.setSize = function ( width, height ) {
 
-    renderer.autoClear = false;
+		_width = width / 2;
+		_height = height;
 
-    this.setSize = function(width, height) {
+		renderer.setSize( width, height );
 
-        _width = width / 2;
-        _height = height;
+	};
 
-        renderer.setSize(width, height);
+	this.render = function ( scene, camera ) {
 
-    };
+		scene.updateMatrixWorld();
 
-    this.render = function(scene, camera) {
+		if ( camera.parent === undefined ) camera.updateMatrixWorld();
+	
+		camera.matrixWorld.decompose( _position, _quaternion, _scale );
 
-        scene.updateMatrixWorld();
+		// Effective fov of the camera
 
-        if (camera.parent === undefined) camera.updateMatrixWorld();
+		_fov = THREE.Math.radToDeg( 2 * Math.atan( Math.tan( THREE.Math.degToRad( camera.fov ) * 0.5 ) / camera.zoom ) );
 
-        camera.matrixWorld.decompose(_position, _quaternion, _scale);
+		_ndfl = camera.near / this.focalLength;
+		_halfFocalHeight = Math.tan( THREE.Math.degToRad( _fov ) * 0.5 ) * this.focalLength;
+		_halfFocalWidth = _halfFocalHeight * 0.5 * camera.aspect;
 
-        // Effective fov of the camera
+		_top = _halfFocalHeight * _ndfl;
+		_bottom = -_top;
+		_innerFactor = ( _halfFocalWidth + this.eyeSeparation / 2.0 ) / ( _halfFocalWidth * 2.0 );
+		_outerFactor = 1.0 - _innerFactor;
 
-        _fov = THREE.Math.radToDeg(2 * Math.atan(Math.tan(THREE.Math.degToRad(camera.fov) * 0.5) / camera.zoom));
+		_outer = _halfFocalWidth * 2.0 * _ndfl * _outerFactor;
+		_inner = _halfFocalWidth * 2.0 * _ndfl * _innerFactor;
 
-        _ndfl = camera.near / this.focalLength;
-        _halfFocalHeight = Math.tan(THREE.Math.degToRad(_fov) * 0.5) * this.focalLength;
-        _halfFocalWidth = _halfFocalHeight * 0.5 * camera.aspect;
+		// left
 
-        _top = _halfFocalHeight * _ndfl;
-        _bottom = -_top;
-        _innerFactor = (_halfFocalWidth + this.eyeSeparation / 2.0) / (_halfFocalWidth * 2.0);
-        _outerFactor = 1.0 - _innerFactor;
+		_cameraL.projectionMatrix.makeFrustum(
+			-_outer,
+			_inner,
+			_bottom,
+			_top,
+			camera.near,
+			camera.far
+		);
 
-        _outer = _halfFocalWidth * 2.0 * _ndfl * _outerFactor;
-        _inner = _halfFocalWidth * 2.0 * _ndfl * _innerFactor;
+		_cameraL.position.copy( _position );
+		_cameraL.quaternion.copy( _quaternion );
+		_cameraL.translateX( - this.eyeSeparation / 2.0 );
 
-        // left
+		// right
 
-        _cameraL.projectionMatrix.makeFrustum(
-        -_outer,
-        _inner,
-        _bottom,
-        _top,
-        camera.near,
-        camera.far
-        );
+		_cameraR.projectionMatrix.makeFrustum(
+			-_inner,
+			_outer,
+			_bottom,
+			_top,
+			camera.near,
+			camera.far
+		);
 
-        _cameraL.position.copy(_position);
-        _cameraL.quaternion.copy(_quaternion);
-        _cameraL.translateX(-this.eyeSeparation / 2.0);
+		_cameraR.position.copy( _position );
+		_cameraR.quaternion.copy( _quaternion );
+		_cameraR.translateX( this.eyeSeparation / 2.0 );
 
-        // right
+		//
 
-        _cameraR.projectionMatrix.makeFrustum(
-        -_inner,
-        _outer,
-        _bottom,
-        _top,
-        camera.near,
-        camera.far
-        );
+		renderer.clear();
+		renderer.enableScissorTest( true );
 
-        _cameraR.position.copy(_position);
-        _cameraR.quaternion.copy(_quaternion);
-        _cameraR.translateX(this.eyeSeparation / 2.0);
+		renderer.setScissor( 0, 0, _width, _height );
+		renderer.setViewport( 0, 0, _width, _height );
+		renderer.render( scene, _cameraL );
 
-        //
+		renderer.setScissor( _width, 0, _width, _height );
+		renderer.setViewport( _width, 0, _width, _height );
+		renderer.render( scene, _cameraR );
 
-        renderer.clear();
-        renderer.enableScissorTest(true);
+		renderer.enableScissorTest( false );
 
-        renderer.setScissor(0, 0, _width, _height);
-        renderer.setViewport(0, 0, _width, _height);
-        renderer.render(scene, _cameraL);
-
-        renderer.setScissor(_width, 0, _width, _height);
-        renderer.setViewport(_width, 0, _width, _height);
-        renderer.render(scene, _cameraR);
-
-        renderer.enableScissorTest(false);
-
-    };
+	};
 
 };
