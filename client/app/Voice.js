826152f310e4ceba8b0e4a97885c11d9f6d9debe
Guido Leandro Maliandi <guido@unboundvr.com>
2015-09-22 00:25:12 -0300

    jscs for voice.js

--- client/app/Voice.js
+++ client/app/Voice.js
@@ -1,31 +1,31 @@
 'use strict';
 
-define(['SocketIO', 'RTCMultiConnection', 'World'], function(io, rtc, world) {
+define(['SocketIO', 'RTCMultiConnection', 'World'], function(io, RTCMultiConnection, world) {
 
     var init = function() {
-        var connection = new rtc('continuum');
+        var connection = new RTCMultiConnection('continuum');
         connection.socketURL = window.location.origin;
-        
+
         connection.session = {
-             audio:     true,
-             video:     false, 
-             screen:    false,
-             
-             data:      false,
-             
-             oneway:    false,
-             broadcast: false
-        };    
-        
+            audio:     true,
+            video:     false,
+            screen:    false,
+
+            data:      false,
+
+            oneway:    false,
+            broadcast: false
+        };
+
         connection.sdpConstraints.mandatory = {
             OfferToReceiveAudio: true,
             OfferToReceiveVideo: false
         };
-   
+
         connection.join('hola');
     };
 
     world.onInit(init);
 
-    return {}
+    return {};
 });
