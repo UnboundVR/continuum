407474eb7f5ce3d9383b2a052df155639ddf4054
Guido Leandro Maliandi <guido@unboundvr.com>
2015-09-28 02:02:56 -0300

    refactor of scene handler

--- server/api/scene-handler.js
+++ server/api/scene-handler.js
@@ -4,20 +4,15 @@ var Promise = require('promise');
 var fs = require('fs');
 var path = require('path');
 var sceneDb = require('../db/scene');
+var readFile = Promise.denodeify(fs.readFile);
 
 var getScene = function(uuid) {
     if (uuid) {
         // Later we will check here if the user has permission to access that scene
         return sceneDb.get(uuid);
     } else {
-        return new Promise(function(resolve, reject) {
-            fs.readFile(path.resolve('db/scene.json'), function(err, res) {
-                if (err) {
-                    reject(err);
-                }
-
-                resolve(JSON.parse(res));
-            });
+        return readFile(path.resolve('db/scene.json')).then(function(res) {
+            return JSON.parse(res);
         });
     }
 };
