ae92ee3a93ad1001cd05ed08527fdf577c7dad86
Guido Leandro Maliandi <guido@unboundvr.com>
2015-09-28 02:13:33 -0300

    WIP remote obj loading

--- server/api/scene-handler.js
+++ server/api/scene-handler.js
@@ -4,17 +4,42 @@ var Promise = require('promise');
 var fs = require('fs');
 var path = require('path');
 var sceneDb = require('../db/scene');
+var traverse = require('../../shared/TraverseTree');
+
 var readFile = Promise.denodeify(fs.readFile);
 
 var getScene = function(uuid) {
-    if (uuid) {
-        // Later we will check here if the user has permission to access that scene
-        return sceneDb.get(uuid);
-    } else {
-        return readFile(path.resolve('db/scene.json')).then(function(res) {
-            return JSON.parse(res);
+    var doGetScene = function(uuid) {
+        if (uuid) {
+            // Later we will check here if the user has permission to access that scene
+            return sceneDb.get(uuid);
+        } else {
+            return readFile(path.resolve('db/scene.json')).then(function(res) {
+                return JSON.parse(res);
+            });
+        }
+    };
+
+    // FIXME remotes are not populated into db yet
+    return doGetScene(uuid).then(function(scene) {
+        if (!scene.remote || !scene.remote.length) {
+            return scene;
+        }
+
+        var promises = [];
+        scene.remote.forEach(function(url) {
+            promises.push(readFile(url));
         });
-    }
+
+        return Promise.all(promises).then(function(results) {
+            delete scene.remote;
+
+            // TODO expand remote objects into scene
+
+            return scene;
+        });
+
+    });
 };
 
 module.exports = {
