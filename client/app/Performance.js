50addc7250600b477c43e0573bcc9d1b0dd47aba
Guido Leandro Maliandi <guido.lm@gmail.com>
2015-10-04 17:37:10 -0300

    filename change

---
+++ client/app/Performance.js
@@ -0,0 +1,30 @@
+define(['World'], function(world) {
+    var fpsList = [];
+    var fps = 0;
+
+    var init = function() {
+        world.onLoop(loop);
+
+        setInterval(calculateFps, 1000);
+    };
+
+    var calculateFps = function() {
+        var sum = function(a, b) {
+          return a + b;
+        };
+        fps = fpsList.length ? fpsList.reduce(sum) / fpsList.length : 0;
+        fpsList.length = 0;
+    };
+
+    var loop = function(time) {
+        fpsList.push(1000.0 / time.delta);
+    };
+
+    world.onInit(init);
+
+    return {
+        currentFps: function() {
+            return fps;
+        }
+    };
+});
