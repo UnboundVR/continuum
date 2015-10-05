0a62dd83dfe44cdb3e2241066389ac99eb4e32a5
Mauro Julián Fernández <mauro@unboundvr.com>
2015-10-04 23:05:19 -0300

    Position tracking on UI for developers

---
+++ client/app/Developer.js
@@ -0,0 +1,24 @@
+'use strict';
+
+define(['text!assets/html/Developer.html', 'text!assets/css/Developer.css', 'utils/BuildHTMLNode', 'Events'], function(html, css, buildHTMLNode, events) {
+    var container;
+    var coords;
+
+    var init = function() {
+        container = document.getElementById(constants.ui.UI_CONTAINER);
+        var element = buildHTMLNode(html, css);
+        container.appendChild(element);
+        coords = container.getElementsByClassName(constants.coords.COORDS_TEXT)[0];
+
+        events.subscribe(events.list.playermoved, updateCoords);
+    };
+
+    var updateCoords = function(position) {
+        console.log(coords);
+        coords.innerHTML = 'X: ' + position.x;
+    };
+
+    return {
+        init: init
+    };
+});
