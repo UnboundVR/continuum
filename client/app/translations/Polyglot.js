473cd75d91194f28e4b9e972a3bf1b901fb7e67c
Guido Leandro Maliandi <guido.lm@gmail.com>
2015-10-13 00:21:00 -0300

    adding locale detection

--- client/app/translations/Polyglot.js
+++ client/app/translations/Polyglot.js
@@ -1,18 +1,16 @@
 var Polyglot = require('node-polyglot');
 
-var locale = 'en_US'; // TODO infer locale
-
 // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
 
 var locales = {
-    en_US: require('./locales/en_US'),
-    es_AR: require('./locales/es_AR')
+    'en-US': require('./locales/en-US'),
+    'es-AR': require('./locales/es-AR')
 };
 
 // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
 
 var polyglot = new Polyglot({
-    phrases: locales[locale]
+    phrases: locales[navigator.language] || locales[defaultLocale]
 });
 
 module.exports = polyglot;
