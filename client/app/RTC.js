5b10810f6c04e45f0662a8b8ae548aacb7d0f5de
Guido Leandro Maliandi <guido.lm@gmail.com>
2015-10-20 21:01:00 -0300

    WIP rtc

--- client/app/RTC.js
+++ client/app/RTC.js
@@ -2,14 +2,31 @@ var world = require('./World');
 var SimpleWebRTC = require('simplewebrtc');
 
 var init = function() {
+    var presenter = false;
+
+    var media = {
+        audio: true,
+        video: false
+    };
+
+    if (presenter) {
+        media.video = true;
+    }
+
     var webrtc = new SimpleWebRTC({
-        localVideoEl: 'localVideo',
-        remoteVideosEl: 'remoteVideo',
-        autoRequestMedia: true
+        localVideoEl: '', // change depending on stuff
+        remoteVideosEl: '', // change depending on stuff
+        autoRequestMedia: true,
+        media: media
     });
 
     webrtc.on('readyToCall', function () {
       webrtc.joinRoom('continuum');
+    });
+
+    webrtc.on('videoAdded', function(video, peer) {
+        peer.videoEl.volume = 0.5;
+        console.log(peer.videoEl.volume)
     });
 };
 
