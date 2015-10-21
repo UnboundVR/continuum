f71c74c713a889673897836eebfe6cb9224fdff5
Guido Leandro Maliandi <guido.lm@gmail.com>
2015-10-20 22:36:41 -0300

    WIP audio regulation

--- client/app/RTC.js
+++ client/app/RTC.js
@@ -1,9 +1,12 @@
 var world = require('./World');
 var SimpleWebRTC = require('simplewebrtc');
 var profile = require('./auth/Profile');
+var playerSync = require('./playerSync/Service');
+
+var webrtc;
 
 var init = function() {
-    var presenter = true;
+    var presenter = false;
 
     var media = {
         audio: true,
@@ -14,7 +17,7 @@ var init = function() {
         media.video = true;
     }
 
-    var webrtc = new SimpleWebRTC({
+    webrtc = new SimpleWebRTC({
         localVideoEl: 'localVideo', // change depending on stuff
         remoteVideosEl: 'remoteVideo', // change depending on stuff
         autoRequestMedia: true,
@@ -30,6 +33,18 @@ var init = function() {
         peer.videoEl.volume = 0.5;
         console.log(peer.nick + ' joined');
     });
+
+};
+
+var updateVolumes = function() {
+    webrtc.getPeers().forEach(function(peer) {
+        if(peer.videoEl) {
+            console.log('peer ' + peer.nick + ' has video', peer);
+            var distance = 2;
+            peer.videoEl.volume = 1 / distance;
+        }
+    });
 };
 
 world.onInit(init);
+world.onLoop(updateVolumes);
