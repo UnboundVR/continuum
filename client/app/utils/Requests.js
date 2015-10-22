901387f90b8c868870e4f635155936b711c7cf04
Guido Leandro Maliandi <guido.lm@gmail.com>
2015-10-21 22:36:27 -0300

    enabling not using authorization header on get requests

--- client/app/utils/Requests.js
+++ client/app/utils/Requests.js
@@ -9,10 +9,13 @@ var toJSON = function(res) {
     return res.json();
 };
 
-var get = function(url) {
+var get = function(url, useAuth) {
+    if(useAuth === undefined) {
+        useAuth = true;
+    }
     return fetch(url, {
         headers: {
-            authorization: consts.auth.BEARER + ' ' + auth.getToken()
+            authorization: useAuth ? consts.auth.BEARER + ' ' + auth.getToken() : undefined
         },
         method: 'GET'
     }).then(toJSON);
